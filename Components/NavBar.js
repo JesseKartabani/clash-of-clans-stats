d2ad10c08a449b1aeff275b8d6005024341fc31a
Jesse1224 <jessekartabani@gmail.com>
2022-11-02 17:07:56 +1100

    Add nav bar

---
+++ Components/NavBar.js
@@ -0,0 +1,57 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  Platform,
+} from "react-native";
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <View style={styles.background}>
+      <SafeAreaView>
+        <View style={styles.navBarContainer}>
+          <Image
+            style={styles.bannerLogo}
+            source={require("../assets/clashLogo.png")}
+          />
+        </View>
+      </SafeAreaView>
+    </View>
+  );
+};
+
+export default NavBar;
+
+const styles = StyleSheet.create({
+  background: {
+    backgroundColor: "#222222",
+  },
+
+  bannerLogo: {
+    flex: 1,
+    width: 120,
+    height: 60,
+    marginLeft: 30,
+    ...Platform.select({
+      ios: {},
+      android: {},
+      default: {
+        width: 175,
+      },
+    }),
+  },
+
+  navBarContainer: {
+    height: 60,
+    ...Platform.select({
+      ios: {},
+      android: {},
+      default: {
+        height: 80,
+      },
+    }),
+  },
+});
