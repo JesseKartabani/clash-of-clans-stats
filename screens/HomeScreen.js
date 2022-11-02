d8ef66c5e56c9795ac54cd70b245b291220cef52
Jesse1224 <jessekartabani@gmail.com>
2022-11-02 17:08:05 +1100

    Add nav bar

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,14 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import NavBar from "../Components/NavBar";
 
 const HomeScreen = () => {
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
+    <NavBar />
+    //
+    //
+    //
+    //
   );
 };
 
