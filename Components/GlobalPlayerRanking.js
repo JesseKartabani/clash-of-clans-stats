602409bea39f40491ce6d5e77964db6630697eba
Jesse1224 <jessekartabani@gmail.com>
2022-11-08 15:05:12 +1100

    Global player ranking styling

--- Components/GlobalPlayerRanking.js
+++ Components/GlobalPlayerRanking.js
@@ -1,4 +1,11 @@
-import { StyleSheet, Text, View, SafeAreaView, Platform } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Platform,
+  Image,
+} from "react-native";
 import React, { useEffect, useState } from "react";
 import { IP } from "@env";
 
@@ -24,15 +31,51 @@ const GlobalPlayerRanking = () => {
 
   useEffect(() => {
     getPlayerStats();
-    //console.log(stats.items[0].name);
   }, []);
-
-  // stats.items[0] = rank 1 player
 
   return (
     <SafeAreaView style={styles.container}>
+      {/* Heading */}
       <Text style={styles.headingText}>Player Rankings</Text>
-      {/*<Text>{stats.items[0].name}</Text> */}
+
+      {/* Top 5 players by trophies */}
+      <View style={styles.catergoryContainer}>
+        <View style={styles.catergoryHeadingContainer}>
+          <Text style={styles.catergoryHeading}>
+            <Image
+              style={styles.trophieImg}
+              source={require("../assets/trophy.jpg")}
+            />
+            Trophies
+          </Text>
+        </View>
+        {/* Name and trophies */}
+        {stats.items != undefined && (
+          <Text style={styles.statTxt}>
+            {stats.items[0].name + ":  " + stats.items[2].trophies}
+          </Text>
+        )}
+        {stats.items != undefined && (
+          <Text style={styles.statTxt}>
+            {stats.items[1].name + ":  " + stats.items[1].trophies}
+          </Text>
+        )}
+        {stats.items != undefined && (
+          <Text style={styles.statTxt}>
+            {stats.items[2].name + ":  " + stats.items[2].trophies}
+          </Text>
+        )}
+        {stats.items != undefined && (
+          <Text style={styles.statTxt}>
+            {stats.items[3].name + ":  " + stats.items[3].trophies}
+          </Text>
+        )}
+        {stats.items != undefined && (
+          <Text style={styles.statTxt}>
+            {stats.items[4].name + ":  " + stats.items[4].trophies}
+          </Text>
+        )}
+      </View>
     </SafeAreaView>
   );
 };
@@ -40,21 +83,96 @@ const GlobalPlayerRanking = () => {
 export default GlobalPlayerRanking;
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "#222222",
-  },
-
-  headingText: {
+  statTxt: {
     color: "white",
-    fontSize: "24px",
-    textAlign: "center",
-    marginTop: 8,
-    marginBottom: 8,
+    fontSize: "16px",
+    paddingLeft: 6,
+    paddingBottom: 8,
     ...Platform.select({
       ios: {},
       android: {},
       default: {
-        fontSize: "160%",
+        fontSize: "18px",
+        marginBottom: 10,
+      },
+    }),
+  },
+
+  catergoryHeadingContainer: {
+    alignItems: "center",
+    //flexDirection: "row",
+    paddingBottom: 10,
+  },
+
+  catergoryHeading: {
+    color: "white",
+    alignSelf: "center",
+    fontWeight: "600",
+    fontSize: "22px",
+    ...Platform.select({
+      ios: {},
+      android: {},
+      default: {
+        fontSize: "24px",
+      },
+    }),
+  },
+
+  trophieImg: {
+    width: 20,
+    height: 20,
+    ...Platform.select({
+      ios: {},
+      android: {},
+      default: {
+        width: 32,
+        height: 32,
+      },
+    }),
+  },
+
+  catergoryContainer: {
+    backgroundColor: "#5A5A5A",
+    width: "50%",
+    height: "50%",
+    paddingTop: 5,
+    borderWidth: 1,
+    borderRadius: 4,
+    borderColor: "gray",
+    ...Platform.select({
+      ios: {},
+      android: {},
+      default: {
+        width: "25%",
+        height: "50%",
+      },
+    }),
+  },
+
+  container: {
+    backgroundColor: "#222222",
+    alignItems: "center",
+    ...Platform.select({
+      ios: {},
+      android: {},
+      default: {
+        height: "100%",
+      },
+    }),
+  },
+
+  headingText: {
+    color: "white",
+    fontSize: "26px",
+    textAlign: "center",
+    marginTop: 10,
+    marginBottom: 10,
+    ...Platform.select({
+      ios: {},
+      android: {},
+      default: {
+        fontSize: "28px",
+        marginBottom: 15,
       },
     }),
   },
