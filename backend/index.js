dc8fb6d781f2326a202b64c5ad26e3456affee24
Jesse1224 <jessekartabani@gmail.com>
2022-11-11 16:49:11 +1100

    mongoose database setup

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,23 @@
 import express from "express";
 import fetch from "node-fetch";
 import cors from "cors";
+import mongoose from "mongoose";
+
+mongoose.connect(
+  "mongodb+srv://clashOfStats:Qa4JEvs0OQOiCWjv@cluster0.8op1t9g.mongodb.net/?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+  }
+);
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("connected to db"));
+
+const userInputSchema = new mongoose.Schema({
+  token: String,
+});
+
+const Token = mongoose.model("Token", userInputSchema);
 
 const API_TOKEN =
   "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjYwMGMzZTMxLWZjZGItNDM0MC1iODVlLTFmMTA1ZTYxZjhhZiIsImlhdCI6MTY2NzMwMzYzOSwic3ViIjoiZGV2ZWxvcGVyL2JkNWRkYTBmLTkxYjEtMzcwZi1jODhlLWZlODQzYzBlY2E4NyIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjExMC4xNDkuMTc5LjYxIl0sInR5cGUiOiJjbGllbnQifV19.WYmZJzeCX1QbghQ-y744BHsljJSWWkPeB4N8zcLVyYf9FhfHuXteBdF36qljc0vIDRw3iRckRHj3dGBuKmY-vQ";
@@ -55,29 +72,41 @@ app.get("/versusTop5", async (req, res) => {
 
 // Get user search input
 app.post("/userSearch", function (req, res) {
-  var newSearch = req.body.token;
-  res.send(newSearch);
-
-  // Use search input to get individual player data
-  app.get("/userSearch", async (req, res) => {
-    try {
-      const response = await fetch(
-        `https://api.clashofclans.com/v1/players/%23${newSearch}`,
-        {
-          headers: {
-            "Content-type": "application/json",
-            Authorization: `Bearer ${API_TOKEN}`,
-          },
-        }
-      );
-      const json = await response.json();
-      const search = json;
-      res.send(search);
-      console.log(search);
-    } catch (error) {
-      console.error(error);
+  const newSearch = req.body.token;
+  Token.findByIdAndUpdate(
+    { _id: "636db4ac5caf3d69fa49ac40" },
+    { $set: { token: `${newSearch}` } },
+    (err) => {
+      if (err) return console.log(err);
     }
-  });
+  );
+});
+
+// Use search input to get individual player data
+app.get("/userSearchResults", async (req, res) => {
+  const userSearch = db
+    .collection("tokens")
+    .find()
+    .toArray(function (err, results) {
+      console.log(results[0].token);
+    });
+  try {
+    const response = await fetch(
+      `https://api.clashofclans.com/v1/players/%23${results[0].token}`,
+      {
+        headers: {
+          "Content-type": "application/json",
+          Authorization: `Bearer ${API_TOKEN}`,
+        },
+      }
+    );
+    const json = await response.json();
+    const search = json;
+    res.send(search);
+    console.log(search);
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 app.listen(port, () => {
