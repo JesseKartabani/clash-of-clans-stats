3d59c1bff83f62d776816ad5db23d0d9080c3645
Jesse1224 <jessekartabani@gmail.com>
2022-11-11 19:31:58 +1100

    Alert if player isnt found then navigate home

--- Components/PlayerSummary.js
+++ Components/PlayerSummary.js
@@ -1,8 +1,10 @@
 import { StyleSheet, Text, View } from "react-native";
 import React, { useState, useEffect } from "react";
 import { IP } from "@env";
+import { useNavigation } from "@react-navigation/native";
 
 const PlayerSummary = () => {
+  const navigation = useNavigation();
   // Player data for users search
   const [userStats, setUserStats] = useState([]);
   const getUserStats = async () => {
@@ -16,6 +18,11 @@ const PlayerSummary = () => {
       const json = await response.json();
       const data = json;
       setUserStats(data);
+      // If player isnt found alert user then navigate to home screen
+      if (data.reason == "notFound") {
+        alert("Player Not Found");
+        navigation.navigate("Clash of Clans Stats");
+      }
       console.log(data);
     } catch (error) {
       console.error(error);
@@ -28,13 +35,11 @@ const PlayerSummary = () => {
 
   return (
     <View>
-      {/* 
       {userStats.name != undefined && (
         <Text style={styles.statTxt}>
           {userStats.name + ":  " + userStats.trophies}
         </Text>
       )}
-      */}
     </View>
   );
 };
