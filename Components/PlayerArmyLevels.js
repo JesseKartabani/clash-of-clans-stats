e1aa326a14d0d4cef84a29237be03c4aeec9aa0c
Jesse1224 <jessekartabani@gmail.com>
2022-11-16 22:44:54 +1100

    Heading and subheading styling

--- Components/PlayerArmyLevels.js
+++ Components/PlayerArmyLevels.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Platform } from "react-native";
 import React, { useState, useEffect } from "react";
 import { IP } from "@env";
 
@@ -27,11 +27,56 @@ const PlayerArmyLevels = () => {
 
   return (
     <View>
-      <Text>PlayerArmyLevels</Text>
+      {/* Heading */}
+      <View style={styles.headingContainer}>
+        <Text style={styles.headingText}> Army </Text>
+      </View>
+
+      {/* Heroes sub heading */}
+      <Text style={styles.subHeadingText}>Heroes</Text>
+
+      {/* All players heroes levels */}
+      <View style={styles.container}>
+        <Text>aaa</Text>
+        <Text>bbb</Text>
+      </View>
     </View>
   );
 };
 
 export default PlayerArmyLevels;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#222",
+    flexDirection: "row",
+  },
+
+  subHeadingText: {
+    paddingLeft: 12,
+    paddingTop: 6,
+    paddingBottom: 6,
+    color: "#FFFFFFB3",
+    fontSize: "16px",
+    backgroundColor: "#222",
+  },
+
+  headingContainer: {
+    backgroundColor: "#121212",
+    height: 60,
+    justifyContent: "center",
+  },
+
+  headingText: {
+    color: "white",
+    textAlign: "center",
+    fontSize: "26px",
+    ...Platform.select({
+      ios: {},
+      android: {},
+      default: {
+        fontSize: "28px",
+      },
+    }),
+  },
+});
