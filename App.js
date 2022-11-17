c0c7e697853dca97510a46fe17752d86918286f3
Jesse1224 <jessekartabani@gmail.com>
2022-11-17 16:18:29 +1100

    Change background color for player stats screen

--- App.js
+++ App.js
@@ -25,6 +25,7 @@ export default function App() {
               component={PlayerStats}
               options={{
                 headerShown: false,
+                cardStyle: { backgroundColor: "#222" },
               }}
             />
           </Stack.Navigator>
